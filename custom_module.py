f39bc8feafb8b5d357ae351cefe3d62fd54c68cf
Ayeni Abiola <ayeniabiola26@gmail.com>
2026-08-11 09:20:47 +0100

    uploading new file

---
+++ custom_module.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    """this function adds two numbers.
+
+    Args:
+        it accepts two arguements: x & y
+        """
+    return x + y
+
+def celsius_to_fahrenheit(celsius):
+    """ this function convery celsius to fahrenheit
+    
+    Args:
+         it accepts one arguement: celsius
+    """
+    return(celsius * 9/5) + 32
+
+name = 'Abiola'
